ea437f770f815b78ae83352ba67e2b6ecfdb7423
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-30 22:29:46 +0200

    copiata funzione per generare una password

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    $lunghezzaPassword = 5 ;
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -40,3 +37,37 @@
         
     </body>
 </html>
+<?php
+    function generateRandomPassword($lunghezzaPassword) {
+        // Caratteri che possono essere utilizzati nella password
+        $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+        $numbers = '0123456789';
+        $symbols = '!@#$%^&*()-_=+[]{};:,.<>?';
+    
+        // Combinare tutti i caratteri disponibili
+        $allCharacters = $uppercase . $lowercase . $numbers . $symbols;
+    
+        // Assicurati che la password contenga almeno un carattere di ciascun tipo
+        $password = '';
+        $password .= $uppercase[random_int(0, strlen($uppercase) - 1)];
+        $password .= $lowercase[random_int(0, strlen($lowercase) - 1)];
+        $password .= $numbers[random_int(0, strlen($numbers) - 1)];
+        $password .= $symbols[random_int(0, strlen($symbols) - 1)];
+    
+        // Riempi il resto della password con caratteri casuali
+        for ($i = 0; $i < $lunghezzaPassword; $i++) {
+            $password .= $allCharacters[random_int(0, strlen($allCharacters) - 1)];
+        }
+    
+        // Mescola la password per evitare sequenze prevedibili
+        $password = str_shuffle($password);
+    
+        return $password;
+    }
+    
+    // Esempio di utilizzo
+    $newPassword = generateRandomPassword($lunghezzaPassword);
+    echo "La tua password casuale è: " . $newPassword;
+    
+?>
