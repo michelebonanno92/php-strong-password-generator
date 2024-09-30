561f07d1ceac1b9e35349174323e515efe58e5a2
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-30 21:35:28 +0200

    inviata lunhgezza password in metodo GET

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
+<?php
+    $lunghezzaPassword = 5 ;
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>php-strong-password-generator</title>
-        <!-- link css -->
-         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
 
-        <header>
-            Hello PHP
-        </header>
+        <main>
+            <section>
+                <form method="GET">
+                        <div>
+                            Lunghezza password :
+                        </div>
+                        <br>
+                        <div>
+                            <button type="submit" name="lunghezzaPassword" value="5">
+                                INVIA
+                            </button>
+                        </div>
+                </form>
+            </section>
+        
+        </main>
         
     </body>
 </html>
